d60273a735db08feb3e80e06e8ebee77d8723a00
Carl D Hamann <carl.hamann@gmail.com>
2012-11-22 11:59:36 -0600

    Move encode logic to a function

--- src/pie.c
+++ src/pie.c
@@ -3,20 +3,27 @@
 
 #define WIDTH 4096
 #define DEPTH 256
-#define OVERHEAD 1
-#define IDXSIZE WIDTH * DEPTH
+#define OVERHEAD 5
 
 struct polynomial
 {
-	unsigned short position;
-	unsigned short depth;
-	unsigned short iterations;
-	unsigned short padding;
+	size_t offset;
+	size_t length;
+	size_t iterations;
 };
 
-static unsigned char table[DEPTH][WIDTH];
-static struct polynomial index[IDXSIZE];
-static unsigned char deltas[WIDTH];
+u_int8_t log8(size_t x)
+{
+	u_int8_t i;
+	for(i = 0; x != 0; i++)
+	{
+		x >>= 8;
+	}
+
+	return i;
+}
+
+static u_int8_t table[DEPTH][WIDTH];
 
 int main(void)
 {
@@ -41,14 +48,14 @@ int main(void)
 			{
 				if(p.iterations == 0)
 				{
-					p.position = j;
+					p.offset = j;
 				}
 
 				p.iterations++;
 			}
-			else if(p.iterations > OVERHEAD && table[i - 1][p.position] != 0)
+			else if(p.iterations > OVERHEAD && table[i - 1][p.offset] != 0)
 			{
-				p.depth = i;
+				p.length = i;
 				index[k++] = p;
 				p.iterations = 0;
 			}
@@ -58,9 +65,9 @@ int main(void)
 			}
 		}
 
-		if(p.iterations > OVERHEAD && table[i - 1][p.position] != 0)
+		if(p.iterations > OVERHEAD && table[i - 1][p.offset] != 0)
 		{
-			p.depth = i;
+			p.length = i;
 			index[k++] = p;
 		}
 
@@ -84,4 +91,59 @@ int main(void)
 	fclose(stdin);
 
 	exit(EXIT_SUCCESS);
+}
+
+size_t encode(const u_int8_t *data, size_t dlength, u_int8_t *output, size_t olength, const u_int8_t *sentinel, size_t slength, size_t depth);
+{
+	u_int8_t **table;
+	u_int8_t *tb;
+	size_t i;
+	size_t j;
+	size_t k;
+	
+	table = calloc(depth, sizeof(*table));
+
+	if(table == NULL)
+	{
+		return 0;
+	}
+
+	tb = calloc(dlength * depth, sizeof(*tb));
+
+	if(tb == NULL)
+	{
+		free(table);
+		return 0;
+	}
+
+	for(i = 0; i < depth; i++)
+	{
+		table[i] = tb + i * dlength;
+	}
+
+	memcpy(&table[0][0], data, dlength);
+
+	for(i = 0; i < dlength && i < depth; i++)
+	{
+		k = i;
+		for(j = 0; j < i; j++)
+		{
+			table[k][j] = table[k - 1][j + 1] - table[k - 1][j];
+			k--;
+		}
+	}
+
+	for(i = depth; i < dlength; i++)
+	{
+		k = depth - 1;
+		for(j = i - depth; j < i; j++)
+		{
+			table[k][j] = table[k - 1][j + 1] - table[k - 1][j];
+			k--;
+		}
+	}
+
+	free(table);
+	free(tb);
+
 }
