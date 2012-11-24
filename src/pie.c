219e1eba209d41f1a7d3e0fd29bc997bdf992fdc
Carl D Hamann <carl.hamann@gmail.com>
2012-11-24 16:00:29 -0600

    Add binary heap for priority queue

--- src/pie.c
+++ src/pie.c
@@ -3,25 +3,21 @@
 
 #define WIDTH 4096
 #define DEPTH 256
-#define OVERHEAD 5
+#define OVERHEAD 6
 
-struct polynomial
+typedef struct polynomial
 {
 	size_t offset;
 	size_t length;
 	size_t iterations;
-};
+} polynomial_s;
 
-u_int8_t log8(size_t x)
-{
-	u_int8_t i;
-	for(i = 0; x != 0; i++)
-	{
-		x >>= 8;
-	}
+DECLARE_BINARYHEAP(polynomial_s)
+DEFINE_BINARYHEAP(polynomial_s)
 
-	return i;
-}
+u_int8_t log8(size_t x);
+int itersort(polynomial_s_p a, polynomial_s_p b);
+size_t encode(const u_int8_t *data, size_t dlength, u_int8_t *output, size_t olength, const u_int8_t *sentinel, size_t slength, size_t depth);
 
 static u_int8_t table[DEPTH][WIDTH];
 
@@ -93,18 +89,78 @@ int main(void)
 	exit(EXIT_SUCCESS);
 }
 
-size_t encode(const u_int8_t *data, size_t dlength, u_int8_t *output, size_t olength, const u_int8_t *sentinel, size_t slength, size_t depth);
+int itersort(polynomial_s_p a, polynomial_s_p b)
+{
+	if(a->iterations < b->iterations)
+	{
+		return 1;
+	}
+	else if(a->iterations > b->iterations)
+	{
+		return -1;
+	}
+	else if(a->depth < b->depth)
+	{
+		return -1;
+	}
+	else if(a->depth > b->depth)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+u_int8_t log8(size_t x)
+{
+	u_int8_t i;
+	for(i = 0; x != 0; i++)
+	{
+		x >>= 8;
+	}
+
+	return i;
+}
+
+size_t encode(const u_int8_t *data, size_t dlength, u_int8_t *output, size_t olength, const u_int8_t *sentinel, size_t slength, size_t depth)
 {
 	u_int8_t **table;
 	u_int8_t *tb;
+
+	polynomial_s **index;
+	polynomial_s *ix;
+
+	polynomial_s p;
+
+	struct polynomial_s_binaryheap heap;
+
 	size_t i;
 	size_t j;
 	size_t k;
+
+	index = calloc(dlength, sizeof(*index));
+
+	if(index == NULL)
+	{
+		return 0;
+	}
+
+	ix = calloc(dlength, sizeof(*ix));
+
+	if(ix == NULL)
+	{
+		free(index);
+		return 0;
+	}
 	
 	table = calloc(depth, sizeof(*table));
 
 	if(table == NULL)
 	{
+		free(index);
+		free(ix);
 		return 0;
 	}
 
@@ -112,6 +168,8 @@ size_t encode(const u_int8_t *data, size_t dlength, u_int8_t *output, size_t ole
 
 	if(tb == NULL)
 	{
+		free(index);
+		free(ix);
 		free(table);
 		return 0;
 	}
@@ -123,12 +181,18 @@ size_t encode(const u_int8_t *data, size_t dlength, u_int8_t *output, size_t ole
 
 	memcpy(&table[0][0], data, dlength);
 
+	heap = polynomial_s_binaryheap(index, dlength, 0, NULL, itersort);
+
 	for(i = 0; i < dlength && i < depth; i++)
 	{
 		k = i;
 		for(j = 0; j < i; j++)
 		{
 			table[k][j] = table[k - 1][j + 1] - table[k - 1][j];
+			if(table[k][j] == 0)
+			{
+			}
+
 			k--;
 		}
 	}
@@ -143,7 +207,8 @@ size_t encode(const u_int8_t *data, size_t dlength, u_int8_t *output, size_t ole
 		}
 	}
 
+	free(index);
+	free(ix);
 	free(table);
 	free(tb);
-
 }
